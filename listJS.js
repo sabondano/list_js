f29dd17e5c42f042d1bbbfe7e705ef5d4fa7c5f3
Sebastian Abondano <sabondano1@gmail.com>
2016-02-27 20:20:53 -0500

    Add recursive nth

--- listJS.js
+++ listJS.js
@@ -20,6 +20,16 @@ function nth(list, number) {
   return listToArray(list)[number];  
 }
 
+// recursive nth
+function nth(list, n) {
+  if (!list)
+    return undefined;
+  else if (n == 0)
+    return list.value
+  else
+    return nth(list.rest, n - 1);
+}
+
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
 console.log(listToArray(arrayToList([10, 20, 30])));
